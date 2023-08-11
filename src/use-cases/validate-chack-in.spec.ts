67fae12b599632f36529be0300d782a20640b578
esbnet <esbnet@gmail.com>
2023-08-11 15:44:15 -0300

    feat: :sparkles: validate check-in time

--- src/use-cases/validate-chack-in.spec.ts
+++ src/use-cases/validate-chack-in.spec.ts
@@ -1,6 +1,6 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
-import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-chack-in'
 let checkInsRepository: InMemoryCheckInsRepository
 
@@ -11,11 +11,11 @@ describe('Validate Check-in Use Case', () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate check in', async () => {
@@ -38,5 +38,23 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'invalid-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40)) // utc-3
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOnMinutesInMs = 1000 * 60 * 21 // 21 minutes
+    vi.advanceTimersByTime(twentyOnMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
