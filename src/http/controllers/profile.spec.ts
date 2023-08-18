c14e098088ba422e41ccbbf0fe032ad617f76fa5
esbnet <esbnet@gmail.com>
2023-08-18 15:32:25 -0300

    feat:

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,40 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Profiel (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'any_name',
+      email: 'esbnet@hotmail.com',
+      password: '123123',
+    })
+
+    const authResonse = await request(app.server).post('/sessions').send({
+      email: 'esbnet@hotmail.com',
+      password: '123123',
+    })
+
+    const { token } = authResonse.body
+
+    const profileResponse = await request(app.server)
+      .get('/profile')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        name: 'esbnet@hotmail.com',
+      }),
+    )
+  })
+})
