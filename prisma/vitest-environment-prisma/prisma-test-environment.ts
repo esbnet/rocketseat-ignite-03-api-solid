a0db3ad93a169ae2924d19631499217b7633a5e4
esbnet <esbnet@gmail.com>
2023-08-18 00:03:21 -0300

    feat: :wrench: create test environment to Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import 'dotenv/config'
+
+import { randomUUID } from 'crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+
+// DATABASE_URL="postgresql://docker:docker@localhost:5432/apisolid?schema=public"
+
+function genarateDatabaseURL(Schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Plese provide DATABASE_URL in your environment variables.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', Schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
+
   async setup() {
-    console.log('Setup ---')
+    const schema = randomUUID()
+    const databaseURL = genarateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('---------> Teardown')
+        // await prisma.$executeRawUnsafe(
+        //   `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        // )
+        // await prisma.$disconnect()
       },
     }
   },
