5ce5b9f98ddb9e1cbfae77ece324cb1e710c8e20
esbnet <esbnet@gmail.com>
2023-08-21 16:46:48 -0300

    feat: :sparkles: create controllers and routes from check-ins use cases

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,17 @@
+import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-chack-in-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInParamsSchema = z.object({
+    checkInId: z.string().uuid(),
+  })
+
+  const { checkInId } = validateCheckInParamsSchema.parse(request.params)
+
+  const validadeCheckInUseCase = makeValidateCheckInUseCase()
+
+  await validadeCheckInUseCase.execute({ checkInId })
+
+  return reply.status(204).send()
+}
